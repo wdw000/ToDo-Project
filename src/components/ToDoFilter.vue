cdd49e3cf7ebb594bf3ab542a10be6303f7306ad
wdw000 <dowonwang01@gmail.com>
2022-04-04 15:14:34 +0900

    add filter list animation

--- src/components/ToDoFilter.vue
+++ src/components/ToDoFilter.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="to-do-filter">
     <ul>
-      <li class="todo-all-click" @click="isType('all')">전체</li>
-      <li class="todo-proceeding-click" @click="isType('progress')">진행</li>
-      <li class="todo-complete-click" @click="isType('done')">완료</li>
+      <li class="todo-all-click" @click="isType('all')">
+        <transition name="spread">
+          <div v-if="filterStatus === 'all'"></div>
+        </transition>
+        전체
+      </li>
+      <li class="todo-proceeding-click" @click="isType('progress')">
+        <transition name="spread">
+          <div v-if="filterStatus === 'progress'"></div>
+        </transition>
+        진행
+      </li>
+      <li class="todo-complete-click" @click="isType('done')">
+        <transition name="spread">
+          <div v-if="filterStatus === 'done'"></div>
+        </transition>
+        완료
+      </li>
     </ul>
   </div>
 </template>
@@ -11,6 +26,11 @@
 <script>
 export default {
   created() {},
+  data() {
+    return {
+      filterStatus: "all",
+    };
+  },
   methods: {
     isType(type) {
       this.filterStatus = type;
@@ -32,6 +52,33 @@ ul {
 }
 ul > li {
   flex: 1;
+  padding: 1rem 0;
   text-align: center;
+  position: relative;
+}
+
+ul > li > div {
+  position: absolute;
+  bottom: 0;
+  height: 0.5rem;
+  width: 100%;
+  background-color: lightgray;
+  border-radius: 0.5rem;
+}
+
+.spread-enter-active {
+  animation: wide 0.3s ease-in;
+}
+
+@keyframes wide {
+  from {
+    left: 50%;
+    width: 0;
+  }
+
+  to {
+    left: 0;
+    width: 100%;
+  }
 }
 </style>
